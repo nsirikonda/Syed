5e52808050b07a65b99ba814ee9e4ca95507924a
root <root@ip-172-31-43-196.us-east-2.compute.internal>
2021-01-03 05:56:24 +0000

    adding script

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+	region = "us-east-2"
+	access_key = "AKIA6KHQQZS2PC5PDQWH"
+	secret_key = "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"
+}
+resource "aws_instance" "test_box" {
+	ami = "ami-0a0ad6b70e61be944"
+	instance_type = "t2.micro"
+	key_name = "New_key"
+	tags = {
+	Name = "test" 
+}
+}
+resource "aws_s3_bucket" "test_bucket" {
+	bucket = "12345naveentestbucket123456789"
+	}
