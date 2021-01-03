7bda34d6b1b597f57ed7ccaae751cd7c14087e9a
nsirikonda <37190474+nsirikonda@users.noreply.github.com>
2021-01-03 11:59:48 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -3,14 +3,7 @@ provider "aws" {
 	access_key = "AKIA6KHQQZS2OGCQKCVO"
 	secret_key = "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"
 }
-resource "aws_instance" "test_box" {
-	ami = "ami-0a0ad6b70e61be944"
-	instance_type = "t2.micro"
-	key_name = "New_key"
-	tags = {
-	Name = "test" 
-}
-}
+
 resource "aws_s3_bucket" "test_bucket" {
 	bucket = "bnasdfgheyekeuyfygrtdrfnsdfhjlsfplo"
 	}
